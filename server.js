468adc48894e89ad7d5838a80c71be3c121da7c8
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-06-20 13:24:38 +0200

    connecting to front-end

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs")
+const cors = require("cors")
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
 
 const database = {
   users: [
@@ -38,7 +40,6 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-    
   if (
     req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password

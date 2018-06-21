d008c6ab2e97f4343958d56397db1f78c2bf589d
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-06-21 16:32:28 +0200

    moved api calls  to backend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+const Clarifai = require("clarifai");
+
+const app = new Clarifai.App({
+  apiKey: "f52acef5f1804535b17a9e7880d7ade9"
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json("unable to work with api"));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db("users")
@@ -11,5 +26,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-  handleImage: handleImage
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
 };
--- server.js
+++ server.js
@@ -42,6 +42,10 @@ app.put("/image", (req, res) => {
   image.handleImage(req, res, db);
 });
 
+app.post("/imageurl", (req, res) => {
+  image.handleApiCall(req, res);
+});
+
 app.listen(3000, () => {
   console.log("app is running on port 3000");
 });

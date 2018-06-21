c2b7ef0598a8625f5855574faa59b32e47db2f2f
kszysieg <krzysztof.a.kobylinski@gmail.com>
2018-06-21 15:00:59 +0200

    code clean-up

--- server.js
+++ server.js
@@ -14,41 +14,34 @@ const db = knex({
   }
 });
 
-// db.select("*")
-//   .from("users")
-//   .then(data => {
-//     console.log(data);
-//   });
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-
-
 
 app.get("/", (req, res) => {
   res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
-  db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email) 
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
     .then(data => {
-      const isValid = bcrypt.compareSync(req.body.password,data[0].hash)
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
       if (isValid) {
-        return db.select('*').from('users')
-        .where('email', '=', req.body.email)
-        .then(user => {
-          res.json(user[0])
-        })
-        .catch(err => res.status(400).json('unable to get useer'))
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then(user => {
+            res.json(user[0]);
+          })
+          .catch(err => res.status(400).json("unable to get useer"));
       } else {
-
-        res.status(400).json('wrong credentials')
+        res.status(400).json("wrong credentials");
       }
-      
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => res.status(400).json("wrong credentials"));
 });
 
 app.post("/register", (req, res) => {
@@ -75,11 +68,9 @@ app.post("/register", (req, res) => {
             res.json(user[0]);
           })
           .then(trx.commit)
-          .catch(trx.rollback)
-      })
-
-  })
-  .catch(err => res.status(400).json("unable to register"));
+          .catch(trx.rollback);
+      });
+  }).catch(err => res.status(400).json("unable to register"));
 });
 
 app.get("/profile/:id", (req, res) => {
